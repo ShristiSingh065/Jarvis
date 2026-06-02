377b8dc0ac60630a124bd060821062e80cd37b88
Mahak S <mahak.0655@gmail.com>
2026-06-02 23:02:54 +0530

    Initial Jarvis assistant

---
+++ main.py
@@ -0,0 +1,53 @@
+import win32com.client
+import webbrowser
+import time
+import musiclib
+import speech_recognition as sr
+recognizer=sr.Recognizer()
+import requests
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+newsapi="371ac7e677c44f55a1318f89de6c8afd"
+
+def speak(text):
+    speaker.Speak(text)
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open instagram" in c.lower():
+        webbrowser.open("https://instagram.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ", 1)[1]
+        link=musiclib.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        r=requests.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=371ac7e677c44f55a1318f89de6c8afd")
+        if r.status_code==200:
+            data=r.json()
+            articles=data.get("articles",[])
+            for article in articles:
+                speak(article['title'])
+if __name__ == "__main__":
+    speak("Initializing..")
+    while True:
+        r=sr.Recognizer()
+        
+        print ("recognizing..")
+        try:
+            with sr.Microphone() as source:
+                print("listening...")
+                audio=r.listen(source,timeout=2,phrase_time_limit=3)
+            word=r.recognize_google(audio)
+            if(word.lower()=="jarvis"):
+                 print("Jarvis detected")
+                 speak("ya")
+                 time.sleep(2)
+                 with sr.Microphone() as source:
+                    print("Jarvis active")
+                    audio=r.listen(source)
+                    command=r.recognize_google(audio)
+                    print("Command heard:", command)
+                    processcommand(command)
+        except Exception as e:
+            print("ERROR:", e)
---
+++ musiclib.py
@@ -0,0 +1,3 @@
+music={"krishna":"https://youtu.be/fbo1JO_Qmy0?si=-g_ZXmEZ6AULrYKM",
+     "best":"https://youtu.be/oafxkMv4xnc?si=G6HDVHQ_af3pmhjd",
+       "attack on titan":"https://youtu.be/WPl10ZrhCtk?si=hHrr3ZrIaMwazmn1"  }
